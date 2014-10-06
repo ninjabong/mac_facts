dbb43d28597b6a1df72e292fa1c0dad454c97b8e
ninjabong <ninjabong@users.noreply.github.com>
2014-10-06 17:21:43 -0400

    renamed mac_sshd.rb mac_sshd_status.rb

--- mac_sshd.rb
+++
@@ -1,9 +0,0 @@
-#mac_sshd.rb
-Facter.add(:mac_sshd) do
-  confine :kernel => "Darwin"
-  setcode do
-    string = Facter::Util::Resolution.exec("/usr/sbin/systemsetup getremotelogin")
-    string
-  end
-end
-
