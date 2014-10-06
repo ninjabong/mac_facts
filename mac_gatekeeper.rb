c6d957feb80b4bb911004034a66ae79e07978fce
ninjabong <ninjabong@users.noreply.github.com>
2014-10-06 17:22:08 -0400

    renamed mac_gatekeeper mac_gatekeeper_status

--- mac_gatekeeper.rb
+++
@@ -1,9 +0,0 @@
-#mac_gatekeeper.rb
-Facter.add(:mac_gatekeeper) do
-  confine :kernel => "Darwin"
-  setcode do
-    string = Facter::Util::Resolution.exec("/usr/sbin/spctl --status")
-    string
-  end
-end
-
