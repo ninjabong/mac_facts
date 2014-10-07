0d6a178a2c30986928f72b1ae1cb94e0d701d6b8
ninjabong <ninja@ninjabong.com>
2014-10-07 00:17:02 -0400

    mac_tmutil_last.rb now shows hours, new days elapsed fact (for dashboarding)

---
+++ mac_tmutil_days_elapsed.rb
@@ -0,0 +1,31 @@
+#mac_tmutil_days_elapsed.rb
+Facter.add(:mac_tmutil_days_elapsed) do
+  confine :kernel => "Darwin"
+  setcode do
+    backup_time = Facter::Util::Resolution.exec("/usr/bin/tmutil latestbackup")
+    t = Time.now()
+    current_time = t.strftime("%Y-%m-%d")
+
+    current_year = current_time[0..3].to_i
+    backup_year = backup_time[-17..-14].to_i
+    elapsed_year = current_year.to_i - backup_year.to_i
+
+    current_month = current_time[5..6].to_i
+    backup_month = backup_time[-12..-11].to_i
+    elapsed_month = current_month.to_i - backup_month.to_i
+
+    current_day = current_time[8..9].to_i
+    backup_day = backup_time[-9..-8].to_i
+    elapsed_days = current_day.to_i - backup_day.to_i
+
+    if elapsed_year > 0
+      backup_elapsed = elapsed_year * 365 # days
+    elsif elapsed_month > 0
+      backup_elapsed = elapsed_month * 30 # days
+    elsif elapsed_days > 0
+      backup_elapsed = elapsed_days       # days
+    end
+   backup_elapsed
+  end
+end
+
--- mac_tmutil_last.rb
+++ mac_tmutil_last.rb
@@ -3,15 +3,11 @@ Facter.add(:mac_tmutil_last) do
   confine :kernel => "Darwin"
   setcode do
     string = Facter::Util::Resolution.exec("/usr/bin/tmutil latestbackup")
-    ## if there's an error connecting to a network volume it errors back
-    ## "Unable to locate machine directory for host." which we literally
-    ## return as a string for the fact.
     if string == "" then
       string = "Unable to locate machine directory for host."
       string
     else
-#      string[-17..-3] # gives us YYYY-mm-DD-HHMM
-      string[-17..-8] # gives us YYYY-mm-DD
+      string[-17..-3] # gives us YYYY-mm-DD-HHMM
     end
   end
 end
