c0f2fff8be7eb4246a269d103e9987f07ca11107
ninjabong <ninja@ninjabong.com>
2014-10-08 22:05:17 -0400

    removed mac_tmutil_dest.rb as it caused tm destination to mount, whoopsies

--- mac_tmutil_dest.rb
+++
@@ -1,9 +0,0 @@
-#mac_tmutil_dest.rb
-Facter.add(:mac_tmutil_dest) do
-  confine :kernel => "Darwin"
-  setcode do
-    string = Facter::Util::Resolution.exec("/usr/bin/tmutil destinationinfo|egrep 'Mount|URL'")
-    string[16..string.length]
-  end
-end
-
