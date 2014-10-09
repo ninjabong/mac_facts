f3d7b45bd88fd375cf73704d9cee674ae30b85ae
ninjabong <ninja@ninjabong.com>
2014-10-08 22:02:31 -0400

    made mac_tmutil_last.rb work on 10.8, 10.9 and 10.10

--- mac_tmutil_last.rb
+++ mac_tmutil_last.rb
@@ -2,8 +2,17 @@
 Facter.add(:mac_tmutil_last) do
   confine :kernel => "Darwin"
   setcode do
-    string = Facter::Util::Resolution.exec("defaults read /Library/Preferences/com.apple.TimeMachine.plist Destinations | grep BACKUP_COMPLETED_DATE")
-    string[35..-3]
+    osver = Facter.value("macosx_productversion_major")
+    if osver == "10.8"
+      string = Facter::Util::Resolution.exec("/usr/bin/defaults read /Library/Preferences/com.apple.TimeMachine.plist Destinations | grep BACKUP_COMPLETED_DATE")
+      string[35..-3]
+    elsif osver == "10.9" or "10.10"
+      string = Facter::Util::Resolution.exec("/usr/bin/defaults read /Library/Preferences/com.apple.TimeMachine.plist Destinations | egrep -B 2 ');' | tail -n2 | head -n1")
+      string[13..-2]
+    else
+      string = "not supported"
+      string
+    end
   end
 end
 
