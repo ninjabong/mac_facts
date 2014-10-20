ebc5bfb770d6fb57d731815b2aa3df44f7a72a92
ninjabong <ninja@ninjabong.com>
2014-10-20 02:21:27 -0400

    reworked the elapsed time stuff to use Time objects, es mucho buenoer

---
+++ mac_tmutil_hrs_elapsed.rb
@@ -0,0 +1,24 @@
+#mac_tmutil_hrs_elapsed.rb
+require 'time'
+Facter.add(:mac_tmutil_hrs_elapsed) do
+  confine :kernel => "Darwin"
+  setcode do
+    osver = Facter.value("macosx_productversion_major")
+    if osver == "10.8"
+      string = Facter::Util::Resolution.exec("/usr/bin/defaults read /Library/Preferences/com.apple.TimeMachine.plist Destinations | grep BACKUP_COMPLETED_DATE")
+      output = string[35..-3]
+    elsif osver == "10.9" or "10.10"
+      string = Facter::Util::Resolution.exec("/usr/bin/defaults read /Library/Preferences/com.apple.TimeMachine.plist Destinations | egrep -B 2 ');' | tail -n2 | head -n1")
+      output = string[13..-2]
+    else
+      output = "not supported"
+    end
+    if Time.parse(output) < Time.now
+      t1 = Time.now
+      t2 = Time.parse(output)
+      output = (((t1.to_i - t2.to_i) / 60) / 60 )
+    end
+    output
+  end
+end
+
